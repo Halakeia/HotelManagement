fea65924c314b388441a72d4d016ad3d8578179b
raissa <fariaferreiraraissa@gmail.com>
2025-05-07 22:39:28 -0300

    feat: adicionado BusinessException para tratamento de regras de negócio

---
+++ src/main/java/hotel/bao/controllers/exception/BusinessException.java
@@ -0,0 +1,12 @@
+package hotel.bao.controllers.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
